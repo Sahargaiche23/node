461f646c52907ef934e39f7049987d7f961eaa7c
Sahargaiche23 <sahargaiche6@gmail.com>
2024-12-18 18:39:31 +0100

    chat

--- newapp4TWIN3-init-main/app.js
+++ newapp4TWIN3-init-main/app.js
@@ -1,11 +1,19 @@
 var express = require('express')
 var userRouter = require('./user/controller')
+var chatRouter = require('./chat/controller')
 var osRouter = require('./os/controller')
 var productRouter = require('./product/controller')
 var mongoose = require('mongoose')
+var path = require('path')
+var {sockeIO} =require('./chat/chatService')
+
+
 var app= express()
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','twig')
 app.use(express.json())
 app.use('/users', userRouter)
+app.use('/chat',chatRouter)
 app.use('/os', osRouter)
 app.use('/products', productRouter)
 mongoose.connect('mongodb://localhost:27017/user-db')
@@ -17,6 +25,7 @@ mongoose.connect('mongodb://localhost:27017/user-db')
         })
 var http = require('http')
 var server = http.createServer(app)
+const io = sockeIO(server)
 server.listen(3009,()=>{
     console.log('server started !');
 })
